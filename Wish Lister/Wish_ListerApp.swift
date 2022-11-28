e9959aa575f152616149f0fe65aa4affc3aec4b4
Trance <xp46290@gmail.com>
2022-11-27 20:55:20 -0500

    Initial Commit

---
+++ Wish Lister/Wish_ListerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wish_ListerApp.swift
+//  Wish Lister
+//
+//  Created by Mark Trance on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Wish_ListerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
